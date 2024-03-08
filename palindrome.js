30e58f67c736b1e158b53e213168f34a445d6103
Afilahimaya <afilahimaya@gmail.com>
2024-03-08 21:04:18 +0100

    Updated output string

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,17 @@
 var str = "racecar";
 var result = "True";
-for(let i=0; i<str.length/2; i++){
-    if(str[i]!==str[str.length-i-1]){
+for(let i=0; i < str.length/2; i++){
+    if(str[i] !== str[str.length-i-1]){
         result = "False";
         //console.log(result);
         break;
     }
 }
+console.log(result)
+if( result === 'True'){
+    console.log('Given string : ', str, 'is palindrome');
+} else {
+    console.log('Given string : ', str, 'is not palindrome');
+}
 
-console.log(result);
     
