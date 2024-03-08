e77ca69ce7e753b3d3610157f215fb9a4b295a70
Afilahimaya <afilahimaya@gmail.com>
2024-03-08 20:57:04 +0100

    first version

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+var str = "racecr";
+var result = "True";
+for(let i=0; i<str.length/2; i++){
+    if(str[i]!==str[str.length-i-1]){
+        result = "False";
+        //console.log(result);
+        break;
+    }
+}
+
+console.log(result);
+    
