e6604c7f82fa9282db4151020135ec2267f8727d
Arpita16 <arpita16.4u@gmail.com>
2024-03-10 08:34:18 +0100

    javascript program

---
+++ Program1.js
@@ -0,0 +1,10 @@
+//Print all numbers from 1 to n using a for loop.
+const prompt = require("prompt-sync")();
+
+var n,i;
+n= prompt("Enter the number you want to print");
+
+for(i=1;i<=n;i++)
+{
+    console.log(i);
+}
